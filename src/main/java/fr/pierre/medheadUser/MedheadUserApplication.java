a445e829c864a1f7fd8d3758981c408805f6a92e
luffyerrot <luffyerrot20@gmail.com>
2024-11-11 18:03:01 +0100

    Initialisation du back pour la gestion des utilisateurs

---
+++ src/main/java/fr/pierre/medheadUser/MedheadUserApplication.java
@@ -0,0 +1,13 @@
+package fr.pierre.medheadUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedheadUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedheadUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pierre/medheadUser/MedheadUserApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pierre.medheadUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedheadUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
